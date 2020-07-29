8836921395e58179074ef2f993dfe7eed957d27c
Lucas <44949284+lucasnc@users.noreply.github.com>
2020-07-28 21:42:13 -0300

    Aula 02 - Roteamento e State

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Menu.css';
 import Button from '../Button';
 
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
-                <img className="Logo" src="https://fontmeme.com/permalink/200728/7d3fa33b772dcc1073e83c7865ea9f23.png" alt="netflix-font" border="0" />
-            </a>
+            <Link to="/">
+                <img src="https://fontmeme.com/permalink/200729/3e93350a5cf1ae5ddb0bbf9e599bd20e.png" alt="netflix-font" border="0" />
+            </Link>
 
-            <Button as="a" className="ButtonLink" href="/">
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">
                 Novo vídeo
             </Button>
         </nav>
