c82dc36a4e7d70b30684dc2f20451f280a7faf21
Lucas <44949284+lucasnc@users.noreply.github.com>
2020-07-29 22:59:00 -0300

    Aula 03 - Formulários e Componentes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -7,7 +7,7 @@ function Menu() {
     return (
         <nav className="Menu">
             <Link to="/">
-                <img src="https://fontmeme.com/permalink/200729/3e93350a5cf1ae5ddb0bbf9e599bd20e.png" alt="netflix-font" border="0" />
+                <img className="Logo" src="https://fontmeme.com/permalink/200730/0f73615c1efddd47ba7766494156224f.png" alt="netflix-font" border="0" />
             </Link>
 
             <Button as={Link} className="ButtonLink" to="/cadastro/video">
--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,11 +1,90 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
+    const defaultValues = {
+        name: '',
+        description: '',
+        color: '#000',
+    }
+
+    const [categories, setCategories] = useState([]);
+    const [values, setValues] = useState(defaultValues);
+
+    function setValue(key, value) {
+        setValues({
+            ...values,
+            [key]: value,
+        })
+    }
+
+    function handleChange(e) {
+        setValue(e.target.getAttribute('name'), e.target.value)
+    }
+
     return (
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+            <form onSubmit={(e) => {
+                e.preventDefault()
+                setCategories([...categories, values])
+
+                setValues(defaultValues)
+            }}>
+                <FormField 
+                    label="Nome"
+                    type="text"
+                    name="name"
+                    value={values.nome}
+                    onChange={handleChange}
+                />
+
+                <FormField 
+                    label="Descrição"
+                    type="textarea"
+                    name="description"
+                    value={values.description}
+                    onChange={handleChange}
+                />
+                {/* <div>
+                    <label>
+                        Descrição
+                        <textarea 
+                            type="text"
+                            name="description"
+                            value={values.description}
+                            onChange={(e) => {
+                                setValue(e.target.getAttribute('name'), e.target.value)
+                            }}
+
+                        />
+                    </label> */}
+
+                <FormField 
+                    label="Cor"
+                    type="color"
+                    name="color"
+                    value={values.color}
+                    onChange={handleChange}
+                />
+
+
+                <button>
+                    Salvar
+                </button>
+            </form>
+
+            <ul>
+                {categories.map((category, index) => {
+                    return (
+                        <li key={`${category}${index}`}>
+                            {category.name}
+                        </li>
+                    );
+                })}
+            </ul>
             <Link to="/">
                 Ir para home
             </Link>
